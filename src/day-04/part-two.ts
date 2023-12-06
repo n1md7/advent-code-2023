01f9e4c67fe81ef63dd1f3b33f49185820cec637
n1md7 <b.kodua@gmail.com>
2023-12-07 01:32:11 +0200

    chore: Day 04 Part 2 DONE

--- src/day-04/part-two.ts
+++ src/day-04/part-two.ts
@@ -1,5 +1,15 @@
-
 import { Solution } from '/src/interfaces/solution';
+import { Counter } from '/src/utils/Counter';
+import { PriorityQueue } from '@datastructures-js/priority-queue';
+
+type Line = {
+  winNums: Set<number>;
+  myNums: number[];
+};
+type Match = {
+  i: number;
+  line: Line;
+};
 
 export class PartTwo implements Solution<number> {
   private readonly input: string[];
@@ -9,7 +19,48 @@ export class PartTwo implements Solution<number> {
   }
 
   solve(): number {
-    return 0;
+    const lines: Line[] = [];
+    const total = new Counter(0);
+    const copies = new PriorityQueue<Match>((a, b) => {
+      if (a.i > b.i) return +1;
+      if (a.i < b.i) return -1;
+
+      return 0;
+    });
+
+    for (const [i, text] of this.input.entries()) {
+      const line = this.parseLine(text);
+      copies.enqueue({ i, line });
+      lines.push(line);
+    }
+
+    while (copies.size()) {
+      total.inc();
+
+      const counter = new Counter(0);
+      const copy = copies.pop();
+
+      for (const num of copy.line.myNums) {
+        if (copy.line.winNums.has(num)) {
+          const idx = copy.i + counter.inc();
+          copies.enqueue({
+            i: idx,
+            line: lines[idx],
+          });
+        }
+      }
+    }
+
+    return total.val;
+  }
+
+  private parseLine(line: string) {
+    const [_card, nums] = line.split(': ');
+    const [winNums, myNums] = nums.split(' | ');
+
+    return {
+      winNums: new Set(winNums.split(' ').filter(Number).map(Number)),
+      myNums: myNums.split(' ').filter(Number).map(Number),
+    };
   }
 }
-
--- tests/unit/day-04/part-two.test.ts
+++ tests/unit/day-04/part-two.test.ts
@@ -1,4 +1,3 @@
-
 import { PartTwo } from '/src/day-04/part-two';
 import { expect, it, describe } from 'vitest';
 import { readFileSync } from 'fs';
@@ -6,16 +5,23 @@ import { cwd } from 'node:process';
 
 const path = cwd() + '/src/day-04/input.txt';
 const input = readFileSync(path, 'utf-8');
-const example = `aa.bb.cc`;
+const example = `
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`;
 
-describe.skip('Day 04 - Part 2', () => {
+describe('Day 04 - Part 2', () => {
   it('should verify example input', () => {
     const payload = example.trim();
     const partOne = new PartTwo(payload);
 
     const result = partOne.solve();
 
-    expect(result).toBe(0);
+    expect(result).toBe(30);
   });
 
   it('should verify real input', () => {
@@ -24,7 +30,6 @@ describe.skip('Day 04 - Part 2', () => {
 
     const result = partOne.solve();
 
-    expect(result).toBe(100);
+    expect(result).toBe(23806951);
   });
 });
-
